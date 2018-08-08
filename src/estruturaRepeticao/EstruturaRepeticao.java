370af18a6e48a8e1799ce9f3b7beeeeb288aad1a
ArchanjoJr <math.calazans56@gmail.com>
2018-08-08 13:22:29 -0300

    add Estrutura Repeticao

---
+++ src/estruturaRepeticao/EstruturaRepeticao.java
@@ -0,0 +1,14 @@
+package estruturaRepeticao;
+import java.util.Scanner;
+public class EstruturaRepeticao {
+
+    public static void main(String[] args){
+        int rep = 8;
+        System.out.println("**********");
+        while(rep >= 0){
+            System.out.println("**      **");
+            rep--;
+        }
+        System.out.println("**********");
+    }
+}
---
+++ src/estruturaRepeticao/EstruturaRepeticaoFor.java
@@ -0,0 +1,11 @@
+package estruturaRepeticao;
+
+public class EstruturaRepeticaoFor {
+    public static void main(String[] args){
+        System.out.println("**********");
+        for (int rep = 0; rep < 8; rep++) {
+            System.out.println("**      **");
+        }
+        System.out.println("**********");
+    }
+}
