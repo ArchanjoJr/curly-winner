a87c21a364f8d0c333801c7805e9cf8020961bb7
archanjojr <math.calazans56@gmail.com>
2018-08-07 22:20:38 -0300

    add quadrado package

---
+++ src/quadrado/ImpressaoQuadradoOvalSetaLosango.java
@@ -0,0 +1,14 @@
+package quadrado;
+
+public class ImpressaoQuadradoOvalSetaLosango {
+    public static void main (String[] args) {
+        System.out.println("**********");
+        System.out.println("**      **");
+        System.out.println("**      **");
+        System.out.println("**      **");
+        System.out.println("**      **");
+        System.out.println("**      **");
+        System.out.println("**      **");
+        System.out.print("**********");
+    }
+}
